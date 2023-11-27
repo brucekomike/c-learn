6a986f9d2d85f80e1a0e0ffcb1f0ba5a2f59defc
brucekomike <brucekomike@gmail.com>
2023-11-27 09:19:23 +0800

    update 1-5

---
+++ src/CPP/1-5/t7-3.1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+        A(int i, int j){
+            a=i; b=j;
+        }
+        void Move(int x, int y){
+            a+=x; b+=y;
+        }
+        void Show(){
+            cout << "("<<a<<","<<b<<")"<<endl;
+        }
+
+    private:
+        int a,b;
+};
+class B: private A{
+    public:
+        B(int i, int j,int k,int l):A(i,j){x=k;y=l;};
+        void Show(){
+            cout<<x<<","<<y<<endl;
+        }
+        void fun(){Move(3,5);}
+        void f1(){A::Show();}
+    private:
+        int x,y;
+};
+
+void main(){
+    A e(1,2);
+    e.Show();
+    B d(3,4,5,6);
+    d.fun();
+    d.Show();
+    d.f1();
+}
